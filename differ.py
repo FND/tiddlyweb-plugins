f0fba62b1794848205984ae18fd60cccaef0b15f
FND <fnd@fnd-osmosoft.(none)>
2009-04-22 14:20:43 +0100

    differ: added basic POST support
    This also led to some refactoring WRT parameter handling.

--- differ.py
+++ differ.py
@@ -12,44 +12,51 @@ POST data (JSON representation of a tiddler) can be used instead of a tiddler
 reference (rev1 or rev2 URL parameter)
 
 To Do:
-* rename to diff
-* POST implementation
+* use /diff
 * diff implementation (various formats?)
 """
 
-from difflib import Differ, HtmlDiff
-
 from tiddlyweb import control
 from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.serializer import Serializer
 from tiddlyweb.web import util as web
 from tiddlyweb.web.http import HTTP400
-from tiddlyweb.serializer import Serializer
+
 
 def init(config):
 	print "initializing differ" # XXX: bad form?
 	# extend urls.map
-	config["selector"].add("/differ", GET=compare, POST=compare)
+	config["selector"].add("/differ", GET=get_request, POST=post_request)
 
 
-def compare(environ, start_response):
+def get_request(environ, start_response):
+	rev1, rev2 = _get_revision_params(environ)
 	store = environ["tiddlyweb.store"]
-	# resolve URL parameters
-	def get_param(name):
-		rev = environ["tiddlyweb.query"].get(name)
-		if rev:
-			rev = _get_tiddler(rev[0], store)
-		return rev
-	rev1 = get_param("rev1")
-	rev2 = get_param("rev2")
-	# resolve POST data
-	# TODO
-	# generate diff
-	content = "<pre>%s</pre>" % diff(rev1, rev2)
+	rev1 = _get_tiddler(rev1, store)
+	rev2 = _get_tiddler(rev2, store)
+	content = compare(rev1, rev2)
 	return _generate_response(content, environ, start_response)
 
 
-def diff(a, b): # TODO
-	return "%s\n\n%s" % (a, b) # DEBUG
+def post_request(environ, start_response):
+	length = int(environ["CONTENT_LENGTH"])
+	rev = environ["wsgi.input"].read(length) # TODO: convert JSON to text serialization
+	rev1, rev2 = _get_revision_params(environ)
+	store = environ["tiddlyweb.store"]
+	if rev1:
+		rev1 = _get_tiddler(rev1, store)
+	else:
+		rev1 = rev
+	if rev2: # XXX: duplication; might result in POST data being used for both revisions
+		rev2 = _get_tiddler(rev2, store)
+	else:
+		rev2 = rev
+	content = compare(rev1, rev2)
+	return _generate_response(content, environ, start_response)
+
+
+def compare(rev1, rev2):
+	return "<pre>%s</pre>" % (rev1, rev2)
 
 
 def _get_tiddler(id, store): # XXX: rename
@@ -91,10 +98,27 @@ def _resolve_identifier(id):
 	return type, name, title, rev
 
 
+def _get_revision_params(environ):
+	"""
+	retrieve tiddler identifiers from query string
+	"""
+	store = environ["tiddlyweb.store"]
+
+	def get_param(name):
+		rev = environ["tiddlyweb.query"].get(name)
+		if rev:
+			rev = rev[0]
+		return rev
+
+	rev1 = get_param("rev1")
+	rev2 = get_param("rev2")
+	return rev1, rev2
+
+
 def _generate_response(content, environ, start_response):
 	serialize_type, mime_type = web.get_serialize_type(environ)
 	cache_header = ("Cache-Control", "no-cache") # ensure accesing latest HEAD revision
 	content_header = ("Content-Type", mime_type)
 	response = [cache_header, content_header]
 	start_response("200 OK", response)
-	return content # XXX: should be list!?
+	return [content]
