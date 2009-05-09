f3bfca47af7423537fb36b185c5d3ab826c7f538
FND <fnd@fnd-osmosoft.(none)>
2009-05-09 15:36:25 +0100

    added devtext to sample config

--- tiddlywebconfig.py
+++ tiddlywebconfig.py
@@ -4,5 +4,6 @@ sample TiddlyWeb configuration for FND's plugins
 
 config = {
 	"system_plugins": ["differ", "proxy"],
-	"proxy_whitelist": ["tiddlyspot.com"]
+	"proxy_whitelist": ["tiddlyspot.com"],
+	"server_store": ["devtext", {"store_root": "store"}]
 }
