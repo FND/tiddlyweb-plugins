6b5446e8ce20a7b165cd2d6f21ce59dfbbc75fce
FND <fnd@oda.(none)>
2009-11-09 15:42:22 +0000

    added coverage script
    adapted from TiddlyWeb core's equivalent
    
    also updated PyPI info

--- instancer/setup.py
+++ instancer/setup.py
@@ -10,11 +10,12 @@ setup(
         namespace_packages = ["tiddlywebplugins"],
         name = "tiddlywebplugins.instancer",
         version = VERSION,
+        url = "http://pypi.python.org/pypi/tiddlywebplugins.instancer",
         description = "A TiddlyWeb plugin to simplify instance management for verticals.",
         long_description = file(os.path.join(os.path.dirname(__file__), "README")).read(),
-        author = "FND",
-        url = "http://pypi.python.org/pypi/tiddlywebplugins.instancer",
-        packages = find_packages(exclude=["test", "twp"]),
         platforms = "Posix; MacOS X; Windows",
-        install_requires = ["setuptools", "tiddlyweb"],
+        author = "FND",
+        author_email = "FNDo@gmx.net",
+        packages = find_packages(exclude=["test", "twp"]),
+        install_requires = ["setuptools", "tiddlyweb"]
 )
