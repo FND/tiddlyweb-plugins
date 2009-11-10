d45240e529c3e73a7abea02b49d1f2d76581efed
FND <fnd@oda.(none)>
2009-11-10 16:13:25 +0000

    minor adjustments in setup.py
    changed summary format, fixed ordering and whitespace

--- instancer/setup.py
+++ instancer/setup.py
@@ -6,15 +6,15 @@ from twp.instancer import __version__
 
 
 setup(
-        namespace_packages = ["tiddlywebplugins"],
-        name = "tiddlywebplugins.instancer",
-        version = __version__,
-        url = "http://pypi.python.org/pypi/tiddlywebplugins.instancer",
-        description = "A TiddlyWeb plugin to simplify instance management for verticals.",
-        long_description = file(os.path.join(os.path.dirname(__file__), "README")).read(),
-        platforms = "Posix; MacOS X; Windows",
-        author = "FND",
-        author_email = "FNDo@gmx.net",
-        packages = find_packages(exclude=["test", "twp"]),
-        install_requires = ["setuptools", "tiddlyweb", "tiddlywebplugins.utils"] # XXX: include optional tiddlywebwiki?
+	name = "tiddlywebplugins.instancer",
+	version = __version__,
+	url = "http://pypi.python.org/pypi/tiddlywebplugins.instancer",
+	description = "TiddlyWeb plugin to simplify instance management for verticals",
+	long_description = file(os.path.join(os.path.dirname(__file__), "README")).read(),
+	platforms = "Posix; MacOS X; Windows",
+	author = "FND",
+	author_email = "FNDo@gmx.net",
+	namespace_packages = ["tiddlywebplugins"],
+	packages = find_packages(exclude=["test", "twp"]),
+	install_requires = ["setuptools", "tiddlyweb", "tiddlywebplugins.utils"] # XXX: include optional tiddlywebwiki?
 )
